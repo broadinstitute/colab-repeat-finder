faf3fd45f0a2ff55a6df18210f3d24f339ca476f
bw2 <ben.weisburd@gmail.com>
2024-03-14 15:56:29 -0400

    initial implementation of allowing interruptions

--- repeat_finder.py
+++ repeat_finder.py
@@ -11,45 +11,111 @@ class RepeatTracker:
     """This class tracks repeats of a single motif size in a given input sequence. It outputs all perfect repeats of
     this that pass filter criteria while scanning the input sequence from left to right"""
 
-    def __init__(self, motif_size, min_repeats, min_span, input_sequence, output_intervals):
+    def __init__(self, motif_size, min_repeats, min_span, max_interruptions, input_sequence, output_intervals, verbose=False):
         """Initialize a RepeatTracker object.
 
         Args:
             motif_size (int): The motif size (in base pairs) that will be tracked by this RepeatTracker.
             min_repeats (int): Only add repeats to the output when there's at least this many repeats in a row.
             min_span (int):  Only add repeats to the output when they span at least this many base pairs.
+            max_interruptions (int): How many bases within a motif are allowed to vary across repeats.
             input_sequence (str): The input sequence.
             output_intervals (dict): A dictionary to store detected repeats. The key is (start_0based, end) and the
                 value is the detected motif.
+            verbose (bool): Print verbose output for debugging.
         """
 
         self.motif_size = motif_size
         self.min_repeats = min_repeats
         self.min_span = min_span
+        self.max_interruptions = max_interruptions
         self.input_sequence = input_sequence
         self.output_intervals = output_intervals
+        self.verbose = verbose
 
-        self.current_position = 0
-        self.run_length = 1
+        self.current_position = 0   # 0-based position in the input sequence
+        self.run_length = 0
+
+        self.current_interrupted_positions_in_motif = set()
+        self.position_of_first_interruption = None
+
+        self.previous_output_interval = None  # 3-tuple (start_0based, end, motif) of the most recent interval added to output_intervals
+
+
+    def log(self, message):
+        """Print a message if verbose output is enabled."""
+        if not self.verbose:
+            return
+
+        start_0based = max(0, self.current_position - self.run_length)
+        motif = self.input_sequence[start_0based : start_0based + self.motif_size]
+        print(f"{message:100s}  || "
+              f"{len(self.input_sequence):,d}bp  [{start_0based}:{self.current_position}], "
+              f"run={self.run_length}, "
+              f"i0={self.current_position}: "
+              f"{(self.current_position - start_0based)/len(motif):0.2f} x {motif} "
+              f"==> {self.input_sequence[start_0based : self.current_position]}")
 
     def advance(self):
-        """Increment current position within the input sequence while updating internal state and recording any detected repeats"""
+        """Increment current position within the input sequence while updating internal state and
+        recording any detected repeats.
+
+        Return False if the end of the input sequence has been reached.
+        
+        Pure repeats: 
+        
+        For pure repeats, the algorithm moves forward 1 base at a time, incrementing run_length until it reaches  
+        a position in the sequence where the current base differs from the base at curret position + motif size.
+        Then, it checks if the base(s) between the few extra bases between the curent position and positions 
+        ahead of the interruption at current position + motif size are still consistent with the current repeat run. 
+        If yes, it adds them. Then, it checks if the current repeat run satisfies filter critera for minimum span and
+        minimum number of repeats. If yes, it adds the repeat run to the output_intervals dictionary. 
+        
+        Interrupted repeats:
+        
+        For interrupted repeats, the algorithm is the same as above. However, when it encounters an interruption, it
+        saves the currrent position within the motif.
+               |
+        AACAACAGCAACAACAACAAC
+           
+        """
 
         seq = self.input_sequence
-        i = self.current_position
-        period = self.motif_size
+        if self.current_position >= len(seq) - self.motif_size:
+            self.log(f"advance() reached end of sequence")
+            return False
 
-        if i >= len(seq) - period:
-            return
-
-        if seq[i] == seq[i + period]:
-            self.current_position += 1
+        if seq[self.current_position] == seq[self.current_position + self.motif_size]:
             self.run_length += 1
-            return
+            self.current_position += 1
+            self.log(f"Adding position {self.current_position} to repeat run")
+            return True
 
-        self.output_interval_if_it_passes_filters()
+        print(f"Position {self.current_position} ({seq[self.current_position]}) doesn't match position {self.current_position + self.motif_size} ({seq[self.current_position + self.motif_size]})")
+
+        if self.run_length > 0:
+            if self.position_of_first_interruption is None:
+                # save the current position
+                self.log(f"Saving position {self.current_position} of the first interruption")
+                self.position_of_first_interruption = self.current_position
+
+            current_position_in_motif = self.run_length % self.motif_size
+            if len(self.current_interrupted_positions_in_motif) < self.max_interruptions:
+                self.log(f"Allow base #{current_position_in_motif + 1} within the motif to vary across repeats.")
+                self.current_interrupted_positions_in_motif.add(current_position_in_motif)
+
+            if current_position_in_motif in self.current_interrupted_positions_in_motif:
+                print(f"Continuing to position {self.current_position + 1} despite interruption at position {current_position_in_motif}")
+                self.run_length += 1
+                self.current_position += 1
+                return True
+
+        if self.run_length > 0:
+            self.output_interval_if_it_passes_filters()
+
+        self.run_length = 0
         self.current_position += 1
-        self.run_length = 1
+        return True
 
     def done(self):
         """Output the last interval if it passes filters"""
@@ -57,31 +123,59 @@ class RepeatTracker:
 
     def output_interval_if_it_passes_filters(self):
         """Check internal state to see if enough repeats have accumulated to output an interval. If yes, add it to
-        the output.
+        the output. self.current_position will be the 0-based position in the input sequence where the interval ends.
         """
-
+        self.log(f"Checking whether to output current repeat run")
         seq = self.input_sequence
-        i = self.current_position
-        period = self.motif_size
 
-        # found mismatch, check if previous run is worth outputing
-        start_0based = i - self.run_length + 1
-        motif = seq[start_0based:start_0based + period]
+        start_0based = self.current_position - self.run_length
+        motif = seq[start_0based : start_0based + self.motif_size]
         if "N" in motif:
             return
 
-        if self.run_length + period - 1 >= self.min_span and self.run_length + period - 1 >= self.min_repeats * period:
-            while i < len(seq) - 1 and seq[i+1] == seq[i+1 - period]:
+        if self.run_length + self.motif_size >= self.min_span and self.run_length + self.motif_size >= self.min_repeats * self.motif_size:
+            # extend the interval to the right up to self.motif_size - 1 bases
+            while self.current_position < len(seq) and (seq[self.current_position] == seq[self.current_position - self.motif_size] or (self.run_length % self.motif_size) in self.current_interrupted_positions_in_motif):
                 self.run_length += 1
-                i += 1
+                self.current_position += 1
+                self.log(f"Extend {motif} repeat to {start_0based}-{self.current_position}: {seq[start_0based:self.current_position]}")
 
-        if self.run_length >= self.min_span and self.run_length >= self.min_repeats * period:
-            end = i + 1
+        if self.run_length >= self.min_span and self.run_length >= self.min_repeats * self.motif_size:
+            end = self.current_position
 
-            previous_motif = self.output_intervals.get((start_0based, end))
-            if previous_motif is None or len(motif) < len(previous_motif):
-                self.output_intervals[(start_0based, end)] = motif
+            motif_previously_detected_at_this_interval = self.output_intervals.get((start_0based, end))
+            if (
+                # if another motif size has already been record for this exact interval by another RepeatTracker,
+                # keep the short motif (eg. replace AAGAAG with AAG)
+                motif_previously_detected_at_this_interval is None or len(motif) < len(motif_previously_detected_at_this_interval)
+            ) and (
+                # only output overlapping intervals if they extend beyond the previously added interval by at least one
+                # repeat unit. This becomes important when allowing interruptions.
+                self.previous_output_interval is None or end - self.previous_output_interval[1] >= self.motif_size
+            ):
+                final_motif = motif
+                for k in self.current_interrupted_positions_in_motif:
+                    final_motif = final_motif[:k] + "N" + final_motif[k+1:]
 
+                if self.verbose: print(f"==> Yes! Adding repeat run to output.")
+                self.output_intervals[(start_0based, end)] = final_motif
+                self.previous_output_interval = (start_0based, end, final_motif)
+            else:
+                if self.verbose: print(f"==> No, this interval signicantly overlaps another, previously detected repeat sequence.")
+        else:
+            if self.verbose: print(f"==> No, this run doesn't span enough bases or have enough repeats of a {self.motif_size} bp motif.")
+
+        # start searching again from the position where the 1st interruption was detected
+
+        if self.position_of_first_interruption is not None:
+            print(f"Jumping back from position {self.current_position} to position of first interruption "
+                  f"({self.position_of_first_interruption - 1})")
+            self.current_position = self.position_of_first_interruption
+            self.position_of_first_interruption = None
+
+        self.run_length = 0
+
+        self.current_interrupted_positions_in_motif.clear()
 
 def shift_string_by(string, shift):
     """Shift a string to the right by a given number of characters (eg. "AGTTT" shifted by 2 becomes "TTAGT")."""
@@ -128,28 +222,35 @@ def detect_repeats(input_sequence, filter_settings, verbose=False):
 
     # generate all intervals
     output_intervals = {}
-    run_trackers = []
+    run_trackers = {}
     for motif_size in range(filter_settings.min_motif_size, filter_settings.max_motif_size + 1):
         run_tracker = RepeatTracker(
             motif_size=motif_size,
             min_repeats=filter_settings.min_repeats,
             min_span=filter_settings.min_span,
+            max_interruptions=filter_settings.max_interruptions_by_motif_size.get(motif_size, 0),
             input_sequence=input_sequence,
             output_intervals=output_intervals,
+            verbose=True,
         )
-        run_trackers.append(run_tracker)
+        run_trackers[motif_size] = run_tracker
 
     position_range = range(len(input_sequence))
     if verbose:
         import tqdm
         position_range = tqdm.tqdm(position_range, unit=" bp", unit_scale=True, total=len(input_sequence))
 
-    for i in position_range:
-        for run_tracker in run_trackers:
-            run_tracker.advance()
+    while run_trackers:
+        run_trackers_that_are_done = []
+        for motif_size, run_tracker in run_trackers.items():
+            advanced = run_tracker.advance()
+            if not advanced:
+                run_tracker.done()
+                run_trackers_that_are_done.append(motif_size)
+                print(f"Done with motif size {motif_size}bp")
 
-    for run_tracker in run_trackers:
-        run_tracker.done()
+        for motif_size in run_trackers_that_are_done:
+            del run_trackers[motif_size]
 
     return [(start_0based, end, motif) for (start_0based, end), motif in sorted(output_intervals.items())]
 
@@ -311,6 +412,9 @@ def main():
         print(f"Wrote results to {output_bed_path}")
 
     elif set(args.input_sequence.upper()) <= set("ACGTN"):
+        if args.interval:
+            parser.error("The --interval option is only supported for FASTA files.")
+
         interval_sequence = args.input_sequence
         if not args.output_prefix:
             args.output_prefix = "repeats"
--- repeat_finder_tests.py
+++ repeat_finder_tests.py
@@ -20,14 +20,16 @@ class RepeatFinderTests(unittest.TestCase):
 			min_motif_size=1,
 			max_motif_size=7,
 			min_repeats=3,
+			max_interruptions_by_motif_size={i: 0 for i in range(1, 8)},
 			min_span=6)
 
 		for motif in "A", "CA", "CAG", "CAGA", "CAGAT", "CAGATT", "CAGATTA", "CAGATTAG":
-			repeats = detect_repeats(6*motif, filter_settings)
+			seq = 6*motif
+			repeats = detect_repeats(seq, filter_settings)
 			if len(motif) <= 7:
-				self.assertEqual(repeats, [(0, 6*len(motif), motif)])
+				self.assertEqual(repeats, [(0, len(seq), motif)], f"Error on sequence: {seq}")
 			else:
-				self.assertEqual(repeats, [])
+				self.assertEqual(repeats, [], f"Error on sequence: {seq}")
 
 		motif = "A"
 		filter_settings.min_motif_size = 2
@@ -35,7 +37,16 @@ class RepeatFinderTests(unittest.TestCase):
 		self.assertEqual(repeats, [(0, 7, "AA")])
 
 		motif1 = "A"
-		motif2 = "AAC"
+		motif2 = "AGAC"
+		motif3 = "AAC"
+		seq = 7*motif1 + 7*motif2 + 10*motif3
+		self.assertEqual(len(seq), 65)
+
 		filter_settings.min_motif_size = 2
-		repeats = detect_repeats(7*motif1 + 10*motif2, filter_settings)
-		self.assertEqual(repeats, [(0, 9, "AA"), (7, 37, "AAC")])
+		filter_settings.max_motif_size = 10
+		repeats = detect_repeats(seq, filter_settings)
+		self.assertEqual(repeats, [(0, 8, "AA"), (7, 36, "AGAC"), (33, 65, "ACA")], f"Error on sequence: {seq}")
+
+		filter_settings.min_motif_size = 1
+		repeats = detect_repeats(seq, filter_settings)
+		self.assertEqual(repeats, [(0, 8, "A"), (7, 36, "AGAC"), (33, 65, "ACA")], f"Error on sequence: {seq}")
